c022d6af6181d8c0011bc3ecd320c40bfb763e84
starboysa <felux47@yahoo.com>
2016-08-27 01:58:18 -0700

    Teleport bullet and gun.

---
+++ project/Knight_time/Assets/scripts/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Sprite))]
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        
+        transform.forward = -camForward;
+    }
+}
