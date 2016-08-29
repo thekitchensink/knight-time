acc03ae82b39d3207ac79b3e241c9efd6f763b70
Matthew Oakes <m.oakes@digipen.edu>
2016-08-29 15:17:13 -0700

    werking

---
+++ project/Knight_time/Assets/scripts/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+    public float speed;
+
+    public float unitLength;
+
+    private float tomove;
+
+	// Use this for initialization
+	void Start () {
+	  tomove = speed / 60.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //fish are food not friends
+        
+    }
+
+
+}
