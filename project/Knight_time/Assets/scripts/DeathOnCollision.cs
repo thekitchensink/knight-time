19c101a50f119d7e8769311df7f7e97f369c496b
Aaron Kitchen <hyperstix26.w.classes@gmail.com>
2016-08-29 00:09:23 -0700

    idk

---
+++ project/Knight_time/Assets/scripts/DeathOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathOnCollision : MonoBehaviour
+{
+    public GameObject ParticleEffect;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(ParticleEffect);
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(ParticleEffect);
+        Destroy(gameObject);
+    }
+}
