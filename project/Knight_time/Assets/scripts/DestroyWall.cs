0eaeb38d8400785fbd402f2acdc246bc6328cae0
Matthew Oakes <m.oakes@digipen.edu>
2016-08-28 19:42:19 -0700

    Destructable walls. Going to add some effects for them being destoryed.

---
+++ project/Knight_time/Assets/scripts/DestroyWall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWall : MonoBehaviour {
+
+    private int health = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+      Base_bullet one = coll.gameObject.GetComponent<Base_bullet>();
+      KillYourself two = coll.gameObject.GetComponent<KillYourself>();
+
+        print("fuck");
+
+      if (one || two)
+      {
+        health--;
+        
+        if(health <= 0)
+            Destroy(gameObject);
+      }
+    }
+
+}
